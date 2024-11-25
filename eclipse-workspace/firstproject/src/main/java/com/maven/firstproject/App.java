2ae97f37703fb7048bdb79c8ffe72975c91260f4
Manojkumar <janagamamanojkumar2004@gmail.com>
2024-11-25 14:00:02 +0530

    mhjgesrkgjlerghej

---
+++ eclipse-workspace/firstproject/src/main/java/com/maven/firstproject/App.java
@@ -0,0 +1,13 @@
+package com.maven.firstproject;
+
+/**
+ * Hello world!y
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
